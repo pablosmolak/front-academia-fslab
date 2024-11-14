9b05d08f3ad9130ed42178e106c697fc8b4643b4
Pablo Smolak <pablosmolak2004@hotmail.com>
2024-11-14 17:03:06 -0400

    arrumando os filtros de curso

--- src/app/(no-auth)/page.js
+++ src/app/(no-auth)/page.js
@@ -1,4 +1,5 @@
 "use client";
+import PaginationComponent from "@/components/paginationComponent";
 import { Button } from "@/components/ui/button";
 import {
     Card,
@@ -10,6 +11,7 @@ import {
 } from "@/components/ui/card"
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { cursoSchema } from "@/src/schemas/cursoSchema";
 import { createURLSearch } from "@/src/utils/createURLSearch";
 import { fetchApi } from "@/src/utils/fetchApi";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -22,29 +24,33 @@ import { z } from "zod";
 export default function paginaInicial({ searchParams }) {
 
     const router = useRouter();
+    const schema = cursoSchema.filtrarCurso
 
-    const { data: cursos, isLoading, isError, error } = useQuery({
-        queryKey: ["getCursos", searchParams],
-        queryFn: async () => {
-            const response = await fetchApi("/cursos", "GET", searchParams);
+    const {
+        data: [cursos, pagina, totalPaginas] = [],
+        isLoading,
+        isError,
+        error } = useQuery({
+            queryKey: ["getCursos", searchParams],
+            queryFn: async () => {
+                const response = await fetchApi("/cursos", "GET", {
+                    searchParams: searchParams,
+                    schema: schema,
+                   // hiddenQuerys: hiddenQuerys
+                });
 
-            if (response.error) {
-                throw response.errors;
-            } else {
-                return response.data;
+                if (response.error) {
+                    throw response.errors;
+                } else {
+                    return [response.data, response.pagina, response.totalPaginas];
+                }
             }
-        }
-    })
+        })
 
-    const schema = z.object({
-        limite: z.string()
-    })
-
-    
     const form = useForm({
         resolver: zodResolver(schema),
         defaultValues: {
-            nome: ""
+            filtro: ""
         }
     })
     console.log(form.formState.errors)
@@ -54,8 +60,6 @@ export default function paginaInicial({ searchParams }) {
 
         router.replace(url);
     }
-
-    // console.log(cursos)
 
     if (isError) {
         return (
@@ -70,70 +74,77 @@ export default function paginaInicial({ searchParams }) {
 
     return (
         <>
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(filtrar)}>
+                    <FormField
+                        control={form.control}
+                        name="filtro"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel htmlFor='nome'>Filtro</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        type="text"
+                                        id="filtro"
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
 
-        <Form {...form}>
-            <form onSubmit={form.handleSubmit(filtrar)}>
-                <FormField
-                    control={form.control}
-                    name="limite"
-                    render={({ field }) => (
-                        <FormItem>
-                            <FormLabel htmlFor='nome'>Nome</FormLabel>
-                            <FormControl>
-                                <Input
-                                type="text"
-                                id="nome"
-                                {...field}
-                                />
-                            </FormControl>
-                            <FormMessage/>
-                        </FormItem>
-                    )}
-                />
+                    <Button>Filtrar</Button>
+                </form>
+            </Form>
 
-                <Button>Filtrar</Button>
-            </form>
-        </Form>
-        
-        <div className="flex flex-wrap justify-center gap-4 m-4 mx-16">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
-                {!isLoading && cursos?.map((data, index) => (
-                    <Card key={index} className="max-w-xs max-h-96"> {/* Limite de largura e altura */}
-                        <CardHeader>
-                            {data.capa ? (
-                                <img
-                                    src={handleImagePath(`/cursos/${data.id}/capa`)}
-                                    alt="Capa do curso"
-                                    className="w-full h-40 object-cover"
-                                />
-                            ) : (
-                                <span
-                                    className="w-full h-40 flex items-center justify-center object-cover">
-                                    Imagem indisponível
-                                </span> // Texto opcional para o caso de não ter imagem
-                            )}
-                        </CardHeader>
-                        <CardContent>
-                            <p>{data.nome}</p>
-                            <p>
-                                {data.instrutores.map((instrutor) => instrutor.nome).join(', ')}
-                            </p>
-                        </CardContent>
-                        <CardFooter>
-                            <Link
-                                href={`/cursos/${data.id}`}
-                                className="bg-yellow-300 w-full flex justify-center items-center"
-                            >
-                                <p>Clique aqui</p>
-                            </Link>
-                        </CardFooter>
-                    </Card>
-                ))}
+            <div className="flex flex-wrap justify-center gap-4 m-4 mx-16">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 justify-center">
+                    {!isLoading && cursos?.map((data, index) => (
+                        <Card key={index} className="max-w-xs max-h-96"> {/* Limite de largura e altura */}
+                            <CardHeader>
+                                {data.capa ? (
+                                    <img
+                                        src={handleImagePath(`/cursos/${data.id}/capa`)}
+                                        alt="Capa do curso"
+                                        className="w-full h-40 object-cover"
+                                    />
+                                ) : (
+                                    <span
+                                        className="w-full h-40 flex items-center justify-center object-cover">
+                                        Imagem indisponível
+                                    </span> // Texto opcional para o caso de não ter imagem
+                                )}
+                            </CardHeader>
+                            <CardContent>
+                                <p>{data.nome}</p>
+                                <p>
+                                    {data.instrutores.map((instrutor) => instrutor.nome).join(', ')}
+                                </p>
+                            </CardContent>
+                            <CardFooter>
+                                <Link
+                                    href={`/cursos/${data.id}`}
+                                    className="bg-yellow-300 w-full flex justify-center items-center"
+                                >
+                                    <p>Clique aqui</p>
+                                </Link>
+                            </CardFooter>
+                        </Card>
+                    ))}
 
+                    {!isLoading && totalPaginas > 1 && (
+                        <PaginationComponent
+                            route={"/"}
+                            currentPage={pagina}
+                            totalPages={totalPaginas}
+                            querys={searchParams}
+                            data-test="pagination-component"
+                        />)}
 
-                {isLoading && (<p>carregando</p>)}
+                    {isLoading && (<p>carregando</p>)}
+                </div>
             </div>
-        </div>
         </>
     )
 }
